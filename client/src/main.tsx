a059f291dd49afc0edb82542cfc1fbed2ba65693
Rosen Filipov <rfilipov010@gmail.com>
2026-05-04 10:01:01 +0300

    Convert app entry to TypeScript
    - Rename App.jsx and main.jsx to .tsx
    - Drop .jsx extensions from internal imports
    - Non-null assertion on getElementById('root')
    - Update index.html script src to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './contexts/UserContext.jsx'
+import { UserProvider } from './contexts/UserContext'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <UserProvider>
       <App />
